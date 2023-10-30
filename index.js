7301b60d8dd8a8619a32d57920382eccf0abbf9d
HunkRowgan <141590122+HunkRowgan@users.noreply.github.com>
2023-10-30 21:18:16 +0100

    ex 2.5 - updated arrays
    - formatted the array properties in each json consistently

--- index.js
+++ index.js
@@ -1,75 +1,81 @@
-const bodyParser = require('body-parser');
 const express = require('express'),
-morgan = require('morgan');
-const app = express();
-const fs = require('fs')
-const path = require('path')
+  morgan = require('morgan'),
+  fs = require('fs'),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
 
+  const app = express();
+
+  // log to log.txt
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 app.use(morgan("combined", { stream: accessLogStream }));
 app.use(morgan('common'));
+
+// express.static
+app.use(express.static('public'));
 
 app.use(bodyParser.json());
 
 let movies = [
   {
-    title: 'Star Wars: The Last Jedi',
-    year: 2017,
-    genre: {
-      name: 'Action',
-      age: '12+'
+    "Title": "Star Wars: The Last Jedi",
+    "Year": 2017,
+    "Genre": {
+      "Name": "Action",
+      "Age": "12+"
     },
-    plot: 'Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.',
-    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg',
-    director: {
-      name: 'Rian Johnson',
-      age: 48,
-      birthYear: 1969
+    "Plot": "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+    "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg",
+    "Director": {
+      "Name": "Rian Johnson",
+      "Age": 48,
+      "Year": 1969
     }
   },
   {
-    title: 'Black Swan',
-    year: 2010,
-    genre: {
-      name: 'Thriller',
-      age: '16+'
+    "Title": "Black Swan",
+    "Year": 2010,
+    "Genre": {
+      "Name": "Thriller",
+      "Age": "16+"
     },
-    plot: 'A committed dancer wins the lead role in a production of Tchaikovskys "Swan Lake" only to find herself struggling to maintain her sanity.',
-    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BNzY2NzI4OTE5MF5BMl5BanBnXkFtZTcwMjMyNDY4Mw@@._V1_SX300.jpg',
-    director: {
-      name: 'Darren Aronofsky',
-      age: 52,
-      birthYear: 1969
+    "Plot": "A committed dancer wins the lead role in a production of Tchaikovskys 'Swan Lake' only to find herself struggling to maintain her sanity.",
+    "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BNzY2NzI4OTE5MF5BMl5BanBnXkFtZTcwMjMyNDY4Mw@@._V1_SX300.jpg",
+    "Director": {
+      "Name": "Darren Aronofsky",
+      "Age": 52,
+      "Year": 1969
     }
   },
   {
-    title: 'Fight Club',
-    year: 1999,
-    genre: {
-      name: 'Drama',
-      age: '16+'
+    "Title": "Fight Club",
+    "Year": 1999,
+    "Genre": {
+      "Name": "Drama",
+      "Age": "16+"
     },
-    plot: 'An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soapmaker, forming an underground fight club that evolves into something much, much more.',
-    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMzFjMWNhYzQtYTIxNC00ZWQ1LThiOTItNWQyZmMxNDYyMjA5XkEyXkFqcGdeQXVyNzkwMjQ5N73E@._V1_SX300.jpg',
-    director: {
-      name: 'David Fincher',
-      age: 60,
-      birthYear: 1962
+    "Plot": "An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soapmaker, forming an underground fight club that evolves into something much, much more.",
+    "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMzFjMWNhYzQtYTIxNC00ZWQ1LThiOTItNWQyZmMxNDYyMjA5XkEyXkFqcGdeQXVyNzkwMjQ5N73E@._V1_SX300.jpg",
+    "Director": {
+      "Name": "David Fincher",
+      "Age": 60,
+      "Year": 1962
     }
   },
   {
-    title: 'The Godfather: Part II',
-    year: 1974,
-    genre: {
-      name: 'Crime, Drama',
-      age: '18+'
+    "Title": "The Godfather: Part II",
+    "Year": 1974,
+    "Genre": {
+      "Name": "Crime, Drama",
+      "Age": "18+"
     },
-    plot: 'The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.',
-    poster: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjZiNzIxNTQtNDc5Zi00YWY1LThkMTctMDgzYjY4YjI1YmQyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-    director: {
-      name: 'Francis Ford Coppola',
-      age: 84,
-      birthYear: 1939
+    "Plot": "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.",
+    "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjZiNzIxNTQtNDc5Zi00YWY1LThkMTctMDgzYjY4YjI1YmQyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+    "Director": {
+      "Name": "Francis Ford Coppola",
+      "Age": 84,
+      "Year": 1939
     }
   }
 ];
@@ -87,7 +93,7 @@ let users = [
   }
 ]
 
-// Create allow users to add new users
+// Create: allow users to add new users
 app.post('/users', (req, res) => {
   const newUser = req.body; //requires bodyParser
   
@@ -99,9 +105,10 @@ app.post('/users', (req, res) => {
   else {
     res.status(400).send('users need names');
   }
+
 })
 
-// Delete allow user to remove user
+// Delete: allow user to remove user
 app.delete('/users/:id', (req, res) => {
   const {id} = req.params; //requires bodyParser
   
@@ -116,7 +123,7 @@ app.delete('/users/:id', (req, res) => {
     }
 })
 
-// Update
+// Update: allow users to update info
 app.put('/users/:id', (req, res) => {
   const {id} = req.params;
   const updatedUser = req.body; //requires bodyParser
@@ -133,9 +140,9 @@ app.put('/users/:id', (req, res) => {
 
 })
 
-// Create allow user to add movie
+// Create: allow user to add movie to their favourites list
 app.post('/users/:id/:movieTitle', (req, res) => {
-  const {id,movieTitle} = req.params; //requires bodyParser
+  const {id, movieTitle} = req.params; //requires bodyParser
   
   let user = users.find( user => user.id == id);
 
@@ -148,9 +155,9 @@ app.post('/users/:id/:movieTitle', (req, res) => {
     }
 })
 
-// Delete allow user to remove movie
+// Delete: allow user to remove movie
 app.delete('/users/:id/:movieTitle', (req, res) => {
-  const {id,movieTitle} = req.params; //requires bodyParser
+  const {id, movieTitle} = req.params; //requires bodyParser
   
   let user = users.find( user => user.id == id);
 
@@ -168,16 +175,21 @@ app.get('/', (req, res) => {
   res.send('Welcome to my movie database!');
 });
 
-// read list of movies
+// Get: read list of movies
 app.get('/movies', (req, res) => {                  
   res.status(200).json(movies);
 });
 
-// read info from movies array when searched by title
+// Get: list all users
+app.get('/users', (req, res) => {
+  res.status(200).json(users);
+});
+
+// Get: read info from movies array when searched by title
 app.get('/movies/:title', (req, res) => {                  
   //const title = req.params.title;
   const {title} = req.params; // this line of code is equal to the one above
-  const movie = movies.find(movie => movie.title.toLowerCase() === title.toLowerCase()); // applies find method to movies array, takes funct as arg. => when true, do sth -> when movieTitle = title, send to const movie var
+  const movie = movies.find(movie => movie.Title.toLowerCase() === title.toLowerCase()); // applies find method to movies array, takes funct as arg. => when true, do sth -> when movieTitle = title, send to const movie var
 
   if (movie) {
     res.status(200).json(movie); //not a return function, add return before res.status for return function
@@ -187,11 +199,11 @@ app.get('/movies/:title', (req, res) => {
   }
 });
 
-// read info from movies array when searched by genre
+// Get: read info about genre when searched by genre
 app.get('/movies/genre/:genreName', (req, res) => {                  
   //const title = req.params.title;
   const {genreName} = req.params; // this line of code is equal to the one above
-  const genre = movies.find(genre => genre.name.toLowerCase() === genreName.toLowerCase()).genre; 
+  const genre = movies.find(genre => movie.Genre.name.toLowerCase() === genreName.toLowerCase()).Genre; 
 
   if (genre) {
     res.status(200).json(genre); //not a return function, add return before res.status for return function
@@ -201,11 +213,11 @@ app.get('/movies/genre/:genreName', (req, res) => {
   }
 });
 
-// read info from movies array when searched by director
+// Get: read info from movies array when searched by director
 app.get('/movies/directors/:directorName', (req, res) => {                  
   //const title = req.params.title;
   const {directorName} = req.params; // this line of code is equal to the one above
-  const director = movies.find(genre => director.name.toLowerCase() === directorName.toLowerCase()).director; 
+  const director = movies.find(genre => movie.Director.name.toLowerCase() === directorName.toLowerCase()).director; 
 
   if (director) {
     res.status(200).json(director); //not a return function, add return before res.status for return function
@@ -216,16 +228,13 @@ app.get('/movies/directors/:directorName', (req, res) => {
 });
 
 
-// express.static
-app.use(express.static('public'));
-
 //error handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
   });
 
-// listen for requests app.listen(port_number, (function)
+// listen for requests app.listen(port_number, (function) called when server starts listening
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
 });
