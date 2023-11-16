d97c0a5301b8d4ae999cafb14da6e160e38699b0
HunkRowgan <141590122+HunkRowgan@users.noreply.github.com>
2023-11-16 19:29:27 +0100

    Ex 2.10

--- index.js
+++ index.js
@@ -26,143 +26,79 @@ app.use(express.static('public'));
 
 app.use(bodyParser.json());
 
+//include CORS
+const cors = require('cors');
+let allowedOrigins = ["*"]; // "*" all domains allowed
+
+//require express-validator
+const { check, validationResult} = require('express-validator');
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 let auth = require('./auth')(app); // import auth.js into project
 const passport = require('passport'); // require passport module
 require('./passport'); // import passport.js
 
-/*let movies = [
-  {
-    "Title": "Star Wars: The Last Jedi",
-    "Year": 2017,
-    "Genre": {
-      "Name": "Action",
-      "Age": "12+"
-    },
-    "Plot": "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
-    "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg",
-    "Director": {
-      "Name": "Rian Johnson",
-      "Age": 48,
-      "Year": 1969
-    }
-  },
-  {
-    "Title": "Black Swan",
-    "Year": 2010,
-    "Genre": {
-      "Name": "Thriller",
-      "Age": "16+"
-    },
-    "Plot": "A committed dancer wins the lead role in a production of Tchaikovskys 'Swan Lake' only to find herself struggling to maintain her sanity.",
-    "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BNzY2NzI4OTE5MF5BMl5BanBnXkFtZTcwMjMyNDY4Mw@@._V1_SX300.jpg",
-    "Director": {
-      "Name": "Darren Aronofsky",
-      "Age": 52,
-      "Year": 1969
-    }
-  },
-  {
-    "Title": "Fight Club",
-    "Year": 1999,
-    "Genre": {
-      "Name": "Drama",
-      "Age": "16+"
-    },
-    "Plot": "An insomniac office worker, looking for a way to change his life, crosses paths with a devil-may-care soapmaker, forming an underground fight club that evolves into something much, much more.",
-    "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMzFjMWNhYzQtYTIxNC00ZWQ1LThiOTItNWQyZmMxNDYyMjA5XkEyXkFqcGdeQXVyNzkwMjQ5N73E@._V1_SX300.jpg",
-    "Director": {
-      "Name": "David Fincher",
-      "Age": 60,
-      "Year": 1962
-    }
-  },
-  {
-    "Title": "The Godfather: Part II",
-    "Year": 1974,
-    "Genre": {
-      "Name": "Crime, Drama",
-      "Age": "18+"
-    },
-    "Plot": "The early life and career of Vito Corleone in 1920s New York City is portrayed, while his son, Michael, expands and tightens his grip on the family crime syndicate.",
-    "Poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjZiNzIxNTQtNDc5Zi00YWY1LThkMTctMDgzYjY4YjI1YmQyL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-    "Director": {
-      "Name": "Francis Ford Coppola",
-      "Age": 84,
-      "Year": 1939
-    }
-  }
-];*/
-
-/*let users = [
-  {
-    id: 1,
-    name: "Abi",
-    favouriteMovies: []
-  },
-  {
-    id: 2,
-    name: "Bob",
-    favouriteMovies: ["Fight Club"]
-  }
-]*/
 
 // Create: allow users to add new users
-/*app.post('/users', (req, res) => {
-  const newUser = req.body; //requires bodyParser
-  
-  if (newUser.name) { //(newUser.name) resolves to true not empty string or null or undefined
-      newUser.id = uuid.v4();
-      users.push(newUser);
-      res.status(201).json(newUser);
-  }
-  else {
-    res.status(400).send('users need names');
-  }
+app.post('/users',
+// check user name is > 5 letters and is alphanumeric and is not empty
+  [
+    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username is required').not().isEmpty(),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail(),
+    check('Bio', 'Bio contains non alphanumeric characters - not allowed.').isAlphanumeric()
+  ], async (req, res) => {
 
-})*/
-app.post('/users', async (req, res) => {
-  await Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
-      } else {
-        Users
-          .create({
-            Username: req.body.Username,
-            Password: req.body.Password,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday,
-            Bio: req.body.Bio, //added
-            FavoriteMovies: req.body.FavoriteMovies //added
-          })
-          .then((user) =>{res.status(201).json(user) })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
-    });
-});
+  // check the validation object for errors
+    let errors = validationResult(req);
 
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+      .then((user) => {
+        if (user) {
+          //If the user is found, send a response that it already exists
+          return res.status(400).send(req.body.Username + ' already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: hashedPassword,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday,
+              Bio: req.body.Bio,
+              FavoriteMovies: req.body.FavoriteMovies //added
+
+            })
+            .then((user) => { res.status(201).json(user) })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).send('Error: ' + error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
 
 // Delete: allow user to remove user
-/*app.delete('/users/:id', (req, res) => {
-  const {id} = req.params; //requires bodyParser
-  
-  let user = users.find( user => user.id == id);
-
-  if (user) {
-    //users = user.filter( user  => user.id != id); // code doesn't work
-    res.status(200).send(`User ${id} has been deleted`);
-    }
-    else {
-      res.status(400).send('no such user');
-    }
-})*/
 app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Users.findOneAndDelete({ Username: req.params.Username })
     .then((user) => {
@@ -178,24 +114,26 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
     });
 });
 
-
 // Update: allow users to update info
-/*app.put('/users/:id', (req, res) => {
-  const {id} = req.params;
-  const updatedUser = req.body; //requires bodyParser
-  
-  let user = users.find( user => user.id == id );
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
+// check user name is > 5 letters and is alphanumeric and is not empty
+[
+  check('Username', 'Username is required').optional().isLength({ min: 5 }),
+  check('Username', 'Username contains non-alphanumeric characters - not allowed.').optional().isAlphanumeric(),
+  check('Password', 'Password contains non-alphanumeric characters - not allowed.').optional().isAlphanumeric(),
+  check('Email', 'Email does not appear to be valid').optional().isEmail(),
+  check('Bio', 'Bio contains non-alphanumeric characters - not allowed.').optional().isAlphanumeric()
+],
 
-  if (user) {
-    user.name = updatedUser.name;
-    res.status(200).json(user);
-  }
-  else {
-    res.status(400).send('no such user');
-  }
 
-})*/
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+async (req, res) => {
+// check validation object
+  let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
   {
     // CONDITION TO CHECK ADDED HERE
     if(req.user.Username !== req.params.Username){
@@ -226,19 +164,6 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), as
 });
 
 // Create: allow user to add movie to their favourites list
-/*app.post('/users/:id/:movieTitle', (req, res) => {
-  const {id, movieTitle} = req.params; //requires bodyParser
-  
-  let user = users.find( user => user.id == id);
-
-  if (user) {
-    user.favouriteMovies.push(movieTitle);
-    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
-    }
-    else {
-      res.status(400).send('no such user');
-    }
-})*/
 app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Users.findOneAndUpdate({ Username: req.params.Username }, {
      $push: { FavoriteMovies: req.params.MovieID }
@@ -254,19 +179,6 @@ app.post('/users/:Username/movies/:MovieID', passport.authenticate('jwt', { sess
 });
 
 // Delete: allow user to remove movie
-/*app.delete('/users/:id/:movieTitle', (req, res) => {
-  const {id, movieTitle} = req.params; //requires bodyParser
-  
-  let user = users.find( user => user.id == id);
-
-  if (user) {
-    user.favouriteMovies = user.favouriteMovies.filter(title => title !== movieTitle);
-    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
-    }
-    else {
-      res.status(400).send('no such user');
-    }
-})*/
 app.delete("/users/:name/movies/:movieID", passport.authenticate('jwt', { session: false }), (req, res) => {
   Users.findOneAndUpdate({Name: req.params.Name}, 
     {$pull: {favoriteMovies: req.params.MovieID}},
@@ -284,9 +196,6 @@ app.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
 });
 
 // Get: read list of movies
-/*app.get('/movies', (req, res) => {                  
-  res.status(200).json(movies);
-});*/
 app.get('/movies', passport.authenticate('jwt', { session: false }), passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
     .then((movies) => {
@@ -299,9 +208,6 @@ app.get('/movies', passport.authenticate('jwt', { session: false }), passport.au
 });
 
 // Get: list all users
-/*app.get('/users', (req, res) => {
-  res.status(200).json(users);
-});*/
 app.get('/users', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Users.find()
     .then((users) => {
@@ -325,18 +231,6 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), as
 });
 
 // Get: read movies by title
-/*app.get('/movies/:title', (req, res) => {                  
-  //const title = req.params.title; 
-  const {title} = req.params; // this line of code is equal to the one above
-  const movie = movies.find(movie => movie.Title.toLowerCase() === title.toLowerCase()); // applies find method to movies array, takes funct as arg. => when true, do sth -> when movieTitle = title, send to const movie var
-
-  if (movie) {
-    res.status(200).json(movie); //not a return function, add return before res.status for return function
-  }
-  else {
-    res.status(400).send('No such movie')
-  }
-});*/
 app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ Title: req.params.Title })
     .then((movie) => {
@@ -350,20 +244,6 @@ app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), asyn
 
 
 // Get: read genre info by genre
-//app.get('/movies/genre/:genreName', (req, res) => {                  
-  /*
-  const {genreName} = req.params; // this line of code is equal to the one above
-  const genre = movies.find(genre => movie.Genre.name.toLowerCase() === genreName.toLowerCase()).Genre; 
-
-  if (genre) {
-    res.status(200).json(genre); //not a return function, add return before res.status for return function // code doesn't work
-    res.status(200).send('genre info')
-  }
-  else {
-    res.status(400).send('No such movie')
-  }*/
-  //res.status(200).send('genre info')
-//});
 app.get('/movies/genre/:Name', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ 'Genre.Name': req.params.Name })
     .then((movie) => {
@@ -376,19 +256,6 @@ app.get('/movies/genre/:Name', passport.authenticate('jwt', { session: false }),
 });
 
 // Get: read info from movies array when searched by director
-/*app.get('/movies/directors/:directorName', (req, res) => {                  
-
-  const {directorName} = req.params; // this line of code is equal to the one above
-  const director = movies.find(genre => movie.Director.name.toLowerCase() === directorName.toLowerCase()).director; 
-
-  if (director) {
-    res.status(200).json(director); //not a return function, add return before res.status for return function
-  }
-  else {
-    res.status(400).send('No such director')
-  }
-  res.status(200).send('director info')
-});*/
 app.get('/movies/director/:Name', passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.findOne({ 'Director.Name': req.params.Name })
     .then((movie) => {
@@ -399,8 +266,6 @@ app.get('/movies/director/:Name', passport.authenticate('jwt', { session: false 
       res.status(500).send('Error: ' + err);
     });
 });
-
-
 
 //error handling
 app.use((err, req, res, next) => {
--- models.js
+++ models.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+//import bcrypt
+const bcrypt = require('bcrypt');
+
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
     Description: {type: String, required: true},
@@ -28,6 +31,14 @@ let userSchema = mongoose.Schema({
     Bio: {type: String, required: true},
     FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
   });
+
+userSchema.statics.hashPassword = (password) => {
+return bcrypt.hashSync(password, 10);
+};
+  
+userSchema.methods.validatePassword = function(password) {
+return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
