aa49c54a119a9f640188d507c8ea86e6948b2aa4
HunkRowgan <141590122+HunkRowgan@users.noreply.github.com>
2023-12-01 22:26:27 +0100

    Re-enable authentication /movies

--- index.js
+++ index.js
@@ -204,7 +204,7 @@ app.get('/', (req, res) => {
 });
 
 // Get: read list of movies
-app.get('/movies', /*passport.authenticate('jwt', { session: false }), passport.authenticate('jwt', { session: false }),*/ async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), passport.authenticate('jwt', { session: false }), async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
